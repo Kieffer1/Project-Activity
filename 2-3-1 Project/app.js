3b7cefe6c0b320a2bb3aa9ecc8ea30c301455ae9
Kieffer1 <renica.rjr23@gmail.com>
2024-10-04 20:41:37 +0800

    added folders part 8

--- 2-3-1 Project/app.js
+++ 2-3-1 Project/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+// body parser module 
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("public"));
 
+app.post('/addstudent', (req, res) => {
+    const idNum = req.body.idnum;
+    const fullName = req.body.fullname;
+    const email = req.body.email;
+    console.log(`ID Number: ${idNum} FullName: ${fullName} Email: ${email}`);
+
+})
 app.get('/', (req, res) => {
     res.render('index');
 })
