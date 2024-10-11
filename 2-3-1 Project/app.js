3333493459136cd28f1552e0ab0d25a4a95d0f15
Kieffer1 <renica.rjr23@gmail.com>
2024-10-11 20:32:22 +0800

    Added folders

--- 2-3-1 Project/app.js
+++ 2-3-1 Project/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const app = express();
 // body parser module 
 const bodyParser = require('body-parser');
-const app = express();
+const conn = require('./conn');
 
 app.set("view engine", "ejs");
 
@@ -10,14 +11,39 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("public"));
 
 app.post('/addstudent', (req, res) => {
+    const id = 0;
     const idNum = req.body.idnum;
     const fullName = req.body.fullname;
     const email = req.body.email;
-    console.log(`ID Number: ${idNum} FullName: ${fullName} Email: ${email}`);
+    // console.log(`ID Number: ${idNum} FullName: ${fullName} Email: ${email}`);
+    const query = `INSERT INTO students VALUES ("${id}", "${idNum}", "${fullName}", "${email}")`;
+    conn.query(query, (err, result) => {
+        if(err) throw err;
 
+        
+        res.send(`
+            <script>
+                alert("1 Student added Successfully");
+                window.location.href="/";
+            </script>
+        `);
+
+    })
+
+    conn.end();
 })
 app.get('/', (req, res) => {
-    res.render('index');
+    const getData = "SELECT * FROM students";
+    conn.query(getData, (err, data) => {
+        if(err) throw err;
+
+        res.render('index', {
+            title: "Ren Gwapo",
+            action: "list",
+            sampledata: data
+        });
+    })
+
 })
 
 app.listen(3000, () => {
